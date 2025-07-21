6985a4fd118322d78c1a926531cd747b5cb9a977
Oreilly Lab <lab@oreilly.com>
2025-07-21 19:31:28 +0000

    change res

--- ch55/sample-app/app.js
+++ ch55/sample-app/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.send('fundamentals of DevOps');
   res.end('Hello, Raja !\n');
 });
 
