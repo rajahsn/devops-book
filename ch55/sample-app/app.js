46e5897d206c5b5cb2f11e72df4804842b32b8e3
Oreilly Lab <lab@oreilly.com>
2025-07-21 19:37:06 +0000

    new cahange

--- ch55/sample-app/app.js
+++ ch55/sample-app/app.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 	res.send('fundamentals of DevOps');
-  res.end('Hello, Raja !\n');
+	
+  res.end('Hssssssello, Raja !\n');
 });
 
 const port = process.env.PORT || 8080;
