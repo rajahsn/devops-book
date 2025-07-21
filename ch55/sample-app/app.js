b2a5d345849281469c8e63a2b86cd0e2376d743f
Oreilly Lab <lab@oreilly.com>
2025-07-21 09:52:16 +0000

    chage response text

--- ch55/sample-app/app.js
+++ ch55/sample-app/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  res.end('Hello, Raja !\n');
 });
 
 const port = process.env.PORT || 8080;
